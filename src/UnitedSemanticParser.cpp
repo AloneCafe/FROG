ef80426262ec1117b316a9ff2d10975c78d6dd3b
AloneCafe <yenyu.loong@foxmail.com>
2021-04-27 19:40:26 +0000

    2021/04/27 bug fix & feature update :art::ok_hand::bug:

---
+++ src/UnitedSemanticParser.cpp
@@ -0,0 +1,60 @@
+#include "UnitedSemanticParser.hpp"
+
+std::string UniSemParser::parse() {
+    std::stringstream ss;
+    std::vector<FileStructurePtr> fileStus;
+    
+    if (!_filenames.empty()) {
+        for (const std::string & extfilename : _extfilenames) {
+            FrontParser p(extfilename);
+            FileStructurePtr pFileStu = p.parse();
+            if (!pFileStu)
+                return "";
+            fileStus.push_back(pFileStu);
+        }
+        
+        for (const std::string & filename : _filenames) {
+            FrontParser p(filename);
+            FileStructurePtr pFileStu = p.parse();
+            if (!pFileStu)
+                return "";
+            fileStus.push_back(pFileStu);
+        }
+        
+        SemParser sp;
+        sp.parse(fileStus);
+        if (sp.hasErr()) {
+            for (const auto & e : sp.getErrList())
+                std::cerr << e.getMsg() << std::endl;
+            return "";
+        }
+        ss << sp._asmk.getContextRef().str();
+        
+    } else {
+        
+        for (const std::string & extfilename : _extfilenames) {
+            FrontParser p(extfilename);
+            FileStructurePtr pFileStu = p.parse();
+            if (!pFileStu)
+                return "";
+            fileStus.push_back(pFileStu);
+        }
+        
+        FrontParser p;
+        FileStructurePtr pFileStu = p.parse();
+        if (!pFileStu)
+            return "";
+        fileStus.push_back(pFileStu);
+        
+        SemParser sp;
+        sp.parse(fileStus);
+        if (sp.hasErr()) {
+            for (const auto & e : sp.getErrList())
+                std::cerr << e.getMsg() << std::endl;
+            return "";
+        }
+        ss << sp._asmk.getContextRef().str();
+    }
+    
+    return ss.str();
+}
--- src/UnitedSemanticParser.hpp
+++ src/UnitedSemanticParser.hpp
@@ -17,67 +17,12 @@ public:
     UniSemParser(const std::vector<std::string> & filenames,
               const std::vector<std::string> & extfilenames)
             : _filenames(filenames), _extfilenames(extfilenames) { }
+            
     UniSemParser() = default;
+    
     ~UniSemParser() = default;
     
-    std::string parse() {
-        std::stringstream ss;
-        std::vector<FileStructurePtr> fileStus;
-        
-        if (!_filenames.empty()) {
-            for (const std::string & extfilename : _extfilenames) {
-                FrontParser p(extfilename);
-                FileStructurePtr pFileStu = p.parse();
-                if (!pFileStu)
-                    return "";
-                fileStus.push_back(pFileStu);
-            }
-            
-            for (const std::string & filename : _filenames) {
-                FrontParser p(filename);
-                FileStructurePtr pFileStu = p.parse();
-                if (!pFileStu)
-                    return "";
-                fileStus.push_back(pFileStu);
-            }
-            
-            SemParser sp;
-            sp.parse(fileStus);
-            if (sp.hasErr()) {
-                for (const auto & e : sp.getErrList())
-                    std::cerr << e.getMsg() << std::endl;
-                return "";
-            }
-            ss << sp._asmk.getContextRef().str();
-            
-        } else {
-            
-            for (const std::string & extfilename : _extfilenames) {
-                FrontParser p(extfilename);
-                FileStructurePtr pFileStu = p.parse();
-                if (!pFileStu)
-                    return "";
-                fileStus.push_back(pFileStu);
-            }
-            
-            FrontParser p;
-            FileStructurePtr pFileStu = p.parse();
-            if (!pFileStu)
-                return "";
-            fileStus.push_back(pFileStu);
-            
-            SemParser sp;
-            sp.parse(fileStus);
-            if (sp.hasErr()) {
-                for (const auto & e : sp.getErrList())
-                    std::cerr << e.getMsg() << std::endl;
-                return "";
-            }
-            ss << sp._asmk.getContextRef().str();
-        }
-        
-        return ss.str();
-    }
+    std::string parse();
 };
 
 #endif
