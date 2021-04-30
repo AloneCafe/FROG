436d644417bc9b61e52aa5518294279eccb78446
AloneCafe <yenyu.loong@foxmail.com>
2021-04-30 14:55:43 +0000

    2021/04/30 更新 & 实现一些文件 🐛🔨:globe_with_meridians:

--- src/GarbageCollector.cpp
+++ src/GarbageCollector.cpp
@@ -1,5 +1,47 @@
-//
-// Created by loong on 2021/4/30.
-//
-
 #include "GarbageCollector.hpp"
+
+
+void GarbageCollector::mark_OPSTACK() {
+    VectorsManager & vecman = _vram._vecman;
+    if (_ops._opStack.size() < sizeof(VectorHandler)) {
+        return;
+    }
+    uint32_t end = _ops._opStack.size() - sizeof(VectorHandler);
+    for (uint32_t i = 0; i <= end; ++i) {
+        VectorHandler probaVH = *reinterpret_cast<const VectorHandler *>(&_ops._opStack[i]);
+        IVector *pVec = vecman.getVectorByHandler(probaVH);
+        if (!pVec)
+            continue;
+        pVec->setMark(true);
+    }
+}
+
+void GarbageCollector::mark_SRAM() {
+    VectorsManager & vecman = _vram._vecman;
+    if (_sram._funcsSRAM.size() < sizeof(VectorHandler)) {
+        return;
+    }
+    uint32_t end = _sram._funcsSRAM.size() - sizeof(VectorHandler);
+    for (uint32_t i = 0; i <= end; ++i) {
+        VectorHandler probaVH = *reinterpret_cast<const VectorHandler *>(&_sram._funcsSRAM[i]);
+        IVector *pVec = vecman.getVectorByHandler(probaVH);
+        if (!pVec)
+            continue;
+        pVec->setMark(true);
+    }
+}
+
+void GarbageCollector::sweep() {
+    VectorsManager & vecman = _vram._vecman;
+    auto it = vecman._map.cbegin();
+    while (it != vecman._map.cend()) {
+        if (!it->second->getMark()) {
+            delete it->second;
+            vecman._map.erase(it);
+        } else {
+            it->second->setMark(false);
+            ++it;
+        }
+    }
+}
+
--- src/GarbageCollector.hpp
+++ src/GarbageCollector.hpp
@@ -50,48 +50,11 @@ public:
     }
     */
     
-    void mark_OPSTACK() {
-        VectorsManager & vecman = _vram._vecman;
-        if (_ops._opStack.size() < sizeof(VectorHandler)) {
-            return;
-        }
-        uint32_t end = _ops._opStack.size() - sizeof(VectorHandler);
-        for (uint32_t i = 0; i <= end; ++i) {
-            VectorHandler probaVH = *reinterpret_cast<const VectorHandler *>(&_ops._opStack[i]);
-            IVector *pVec = vecman.getVectorByHandler(probaVH);
-            if (!pVec)
-                continue;
-            pVec->setMark(true);
-        }
-    }
+    void mark_OPSTACK();
     
-    void mark_SRAM() {
-        VectorsManager & vecman = _vram._vecman;
-        if (_sram._funcsSRAM.size() < sizeof(VectorHandler)) {
-            return;
-        }
-        uint32_t end = _sram._funcsSRAM.size() - sizeof(VectorHandler);
-        for (uint32_t i = 0; i <= end; ++i) {
-            VectorHandler probaVH = *reinterpret_cast<const VectorHandler *>(&_sram._funcsSRAM[i]);
-            IVector *pVec = vecman.getVectorByHandler(probaVH);
-            if (!pVec)
-                continue;
-            pVec->setMark(true);
-        }
-    }
+    void mark_SRAM();
     
-    void sweep() {
-        VectorsManager & vecman = _vram._vecman;
-        auto it = vecman._map.cbegin();
-        while (it != vecman._map.cend()) {
-            if (!it->second->getMark()) {
-                delete it->second;
-                vecman._map.erase(it);
-            } else {
-                ++it;
-            }
-        }
-    }
+    void sweep();
 };
 
 #endif
